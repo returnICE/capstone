ad934a1739fcacd0f594506796fce5521dbe69d2
jeky22 <qhzh33@gmail.com>
2020-02-21 10:52:44 +0900

    dev #La01 - swaggger 환경세팅

--- BE/app.js
+++ BE/app.js
@@ -11,13 +11,13 @@ var flash = require('connect-flash');
 
 //DB 연결
 var mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://turrymall:turrymall@cluster0-7wnzw.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://turrymall:turrymall@cluster0-7wnzw.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
-db.once("open",function(){
+db.once("open", function () {
   console.log("DB connected");
 });
-db.on("error",function(err){
-  console.log("DB ERROR : ",err);
+db.on("error", function (err) {
+  console.log("DB ERROR : ", err);
 });
 
 require('./models/user.js');
@@ -41,12 +41,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(methodOverride("_method"));
 app.use(session({
-  key:'sid',
-  secret:'TURRYMALL',
+  key: 'sid',
+  secret: 'TURRYMALL',
   resave: false,
   saveUninitialized: true,
-  cookie:{
-    maxAge: 24000*60*60
+  cookie: {
+    maxAge: 24000 * 60 * 60
   }
 })); //세션 설정
 app.use(passport.initialize());
@@ -56,24 +56,54 @@ app.use(flash());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+// Swagger definition
+// https://lahuman.github.io/nodejs_swagger/
+// You can set every attribute except paths and swagger
+// https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md
+const swaggerDefinition = {
+  info: { // API informations (required)
+    title: 'Auth Service', // Title (required)
+    version: '1.0.0', // Version (required)
+    description: 'Auth API' // Description (optional)
+  },
+  host: 'localhost:3000', // Host (optional)
+  basePath: '/' // Base path (optional)
+};
+
+// Options for the swagger docs
+const options = {
+  // Import swaggerDefinitions
+  swaggerDefinition,
+  // Path to the API docs
+  apis: ['./routes/index.js', './users/index.js']
+};
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 var User = require('mongoose').model('User');
-passport.serializeUser(function(user,done){
-  done(null,user.id);
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
 }); // session 생성 시 user개체의 id(DB의 id)를 저장
 
-passport.deserializeUser(function(id,done){
-  User.findById(id,function(err,user){
-    done(err,user);
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
   });
 }); // session으로부터 개체 가져올 때 id를 넘겨받아서 DB에서 user찾음
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -82,5 +112,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
