a1206d58faad587f8b78b22aa4740887834ef2cd
하태민 <ht4380@ajou.ac.kr>
2020-03-15 08:35:31 +0900

    사진 업로드 기능 틀만 구현

--- ANDROID/app/src/main/java/com/capstone/androidproject/ItemRegisterActivity.kt
+++ ANDROID/app/src/main/java/com/capstone/androidproject/ItemRegisterActivity.kt
@@ -6,12 +6,19 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Toast
 import androidx.recyclerview.widget.*
 import com.capstone.androidproject.RegisterItem.photoAdapter
 import com.capstone.androidproject.RegisterItem.photoList
+import com.capstone.androidproject.Response.Success
+import com.capstone.androidproject.ServerConfig.ServerConnect
+import com.capstone.androidproject.SharedPreferenceConfig.App
 import kotlinx.android.synthetic.main.activity_item_register_photo.*
 import kotlinx.android.synthetic.main.photo_list.*
 import kotlinx.android.synthetic.main.activity_signup.btnNext
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 import java.lang.Exception
 
@@ -81,5 +88,34 @@ class ItemRegisterActivity : AppCompatActivity() {
             }
         }
         //필요시 else로 로그보는 코드 추가
+    }
+
+    //이미지 업로드 함수 준비
+    fun uploadImg(img : String){
+        val serverConnect = ServerConnect(this)
+        val server = serverConnect.conn()
+
+        server.postItemImgRequest(img).enqueue(object:
+
+            Callback<Success> {
+            override fun onFailure(call: Call<Success>, t: Throwable) {
+                Toast.makeText(this@ItemRegisterActivity, "회원가입 실패1", Toast.LENGTH_SHORT).show()
+                println(t?.message.toString())
+            }
+
+            override fun onResponse(
+                call: Call<Success>,
+                response: Response<Success>
+            ) {
+                val success = response?.body()?.success
+
+                if (success == false) {
+                    Toast.makeText(this@ItemRegisterActivity, "사진전송 실패", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@ItemRegisterActivity, "사진전송 성공", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        })
     }
 }
